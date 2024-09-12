b34606971f98fdd9623bbe8ebe207dcccdbc7128
Moussaoui Yacine <contact@yacinemoussaoui.com>
2024-09-12 21:33:13 +0200

    correction du style d'un des titre

--- components/Features.vue
+++ components/Features.vue
@@ -2,7 +2,7 @@
   <section class="w-full">
     <div class="mx-auto flex flex-col gap-6 justify-center items-center">
       <h2
-        class="text-customGray bg-white opacity-10 backdrop-blur-lg w-full max-w-xs text-5xl text-center tracking-tighter font-medium leading-tight"
+        class="text-customGray w-full max-w-xs text-5xl text-center tracking-tighter font-medium leading-tight"
       >
         Features that work for your future.
       </h2>
